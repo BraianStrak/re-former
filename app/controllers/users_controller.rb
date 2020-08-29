2397432bb9a7f5e38cf24d811c659268bbf070de
BraianStrak <braianstrak1999@gmail.com>
2020-08-29 19:25:13 +0100

    created HTML user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,25 @@ class UsersController < ApplicationController
     end
 
     def create
+        #this creates a user with the parameters, 
+        #if a user saved successfully, it redirects back to new user form,
+        #which will be blank. If you don't, it will render the new form
+        #again
+        # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        
+        @user = User.new(user_params)
+
+        if @user.save
+        redirect_to new_user_path
+        else
+        render :new
+        end
+    end
+
+    private
+    def user_params
+        #whitelists those three parameters, deletes everything else.
+        #returns the parameters
+        params.require(:user).permit(:username, :password, :email)
     end
 end
