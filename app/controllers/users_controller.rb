218ac2d859557840f8491840aec33a1de518d0a8
BraianStrak <braianstrak1999@gmail.com>
2020-08-29 22:36:46 +0100

    User edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     def new
+        @user = User.new
     end
 
     def create
@@ -7,7 +8,7 @@ class UsersController < ApplicationController
         #if a user saved successfully, it redirects back to new user form,
         #which will be blank. If you don't, it will render the new form
         #again
-        # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
         
         @user = User.new(user_params)
 
@@ -16,6 +17,11 @@ class UsersController < ApplicationController
         else
         render :new
         end
+    end
+
+    def edit
+        #find a user based on the submitted params id
+        @user = User.find(params[:id])
     end
 
     private
